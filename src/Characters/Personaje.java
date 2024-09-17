b72a11993529124f77ad7a19fc71cea958bb7687
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-17 09:33:22 +0200

    personaje class added final

--- src/Characters/Personaje.java
+++ src/Characters/Personaje.java
@@ -70,4 +70,92 @@ public abstract class Personaje {
     public abstract void realizarAcciones(PrintWriter escribidor);
 
 
+
+    public void mover(PrintWriter escribidor) {
+        CentralNuclear central = CentralNuclear.getInstancia();
+
+
+        int posicion_actual = this.idCeldaActual;
+        int posicion_siguiente = calcularSiguienteIdCelda();
+
+        if (central.hayCamino(posicion_actual, posicion_siguiente)) {
+            Celda celdaActual = central.getCelda(Utilidad.calcularFila(posicion_actual), Utilidad.calcularColumna(posicion_actual));
+            Celda celdaSiguiente = central.getCelda(Utilidad.calcularFila(posicion_siguiente), Utilidad.calcularColumna(posicion_siguiente));
+
+            celdaActual.borrarPersonajePorNombre(this.getNombre());
+
+            setIdCeldaActual(posicion_siguiente);
+
+            celdaSiguiente.insPersonaje(this);
+
+
+        }else {
+            escribidor.println(this.getNombre() + Constantes.NO_PUEDO + Constantes.PARENTESIS1 + (this.lRuta.get(0)) + Constantes.PARENTESIS2);
+
+
+        }
+        borrarPrimerMovimiento();
+
+    }
+
+    public void cargarMovimientos(char[] movimientos) {
+        for (char movimiento : movimientos) {
+            lRuta.add(movimiento);
+
+        }
+    }
+
+    public void borrarPrimerMovimiento() {
+        try {
+            lRuta.remove(0);
+        }
+        catch(IndexOutOfBoundsException e) {
+
+        }
+    }
+
+    public void añadirMovimiento(char movimiento) {
+        lRuta.add(movimiento);
+    }
+
+    public boolean estaVacia() {
+        return lRuta.isEmpty();
+
+    }
+
+    public int calcularSiguienteIdCelda() {
+        int siguienteCelda = -1;
+
+
+
+        if(!estaVacia()) {
+            char primerMovimiento = lRuta.get(0);
+            if (primerMovimiento == 'N') {
+                siguienteCelda = idCeldaActual - 8;
+            } else if (primerMovimiento == 'S') {
+                siguienteCelda = idCeldaActual + 8;
+            } else if (primerMovimiento == 'E') {
+                siguienteCelda = idCeldaActual + 1;
+            } else if (primerMovimiento == 'O') {
+                siguienteCelda = idCeldaActual - 1;
+            }
+
+        }
+
+        return siguienteCelda;
+
+
+    }
+
+
+
+    @Override
+    public String toString() {
+        return nombre+ Constantes.ESTA_EN_CELDA + idCeldaActual + Constantes.ES_TURNO+ turno+ Constantes.LA_MARCA+ marca ;
+    }
+
+
+
+
+
 }
