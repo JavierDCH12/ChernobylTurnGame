9c6337ba3d0664e755a682904f00563b4a5fd5c0
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-18 13:08:05 +0200

    volunatario class added

---
+++ src/Characters/Voluntario.java
@@ -0,0 +1,43 @@
+package Characters;
+
+
+public class Voluntario extends KGB {
+
+    public Voluntario() {
+        super();
+    }
+
+    public Voluntario(String nombre, int turno, int idCeldaActual, char marca) {
+        super(nombre, turno, idCeldaActual, marca);
+    }
+
+
+
+    public void catalogar(PrintWriter escribidor) {
+        int posicion_actual = getIdCeldaActual();
+        CentralNuclear central = CentralNuclear.getInstancia();
+        Celda celdaActual = central.getCelda(Utilidad.calcularFila(posicion_actual), Utilidad.calcularColumna(posicion_actual));
+
+        for (Personaje cadapersonaje : celdaActual.getLista_personajes()) {
+            if (cadapersonaje instanceof Operador && !((Operador) cadapersonaje).isCatalogado()) {
+                String tipoOperador = "";
+                if (cadapersonaje instanceof Cientifico) {
+                    tipoOperador = "CIENTIFICO";
+                } else if (cadapersonaje instanceof Minero) {
+                    tipoOperador = "MINERO";
+                } else if (cadapersonaje instanceof Bombero) {
+                    tipoOperador = "BOMBERO";
+                } else if (cadapersonaje instanceof Oficial) {
+                    tipoOperador = "OFICIAL";
+                } else if (cadapersonaje instanceof Voluntario) {
+                    tipoOperador = "VOLUNTARIO";
+                } else if (cadapersonaje instanceof Robot) {
+                    tipoOperador = "ROBOT";
+                }
+
+                escribidor.println(getNombre() + Constantes.DOS_PUNTOS + tipoOperador + Constantes.ALMOHADILLA + cadapersonaje.getNombre() + Constantes.ALMOHADILLA + cadapersonaje.getIdCeldaActual() + Constantes.ALMOHADILLA + cadapersonaje.getMarca());
+                ((Operador) cadapersonaje).setCatalogado(true);
+            }
+        }
+    }
+}
