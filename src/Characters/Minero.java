15ecca8258933f5b9476552baa4ade9506386827
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-19 11:04:10 +0200

    minero class added methods

--- src/Characters/Minero.java
+++ src/Characters/Minero.java
@@ -51,5 +51,41 @@ public class Minero extends Operador {
     }
 
 
+    public void desescombrar(PrintWriter escribidor) {
+        int posicion_actual = getIdCeldaActual();
+        CentralNuclear central = CentralNuclear.getInstancia();
+        Celda celdaActual = central.getCelda(Utilidad.calcularFila(posicion_actual), Utilidad.calcularColumna(posicion_actual));
+        int cant_escombros_esta_celda = celdaActual.getEscombros();
+
+        if (cant_escombros_esta_celda > 0) {
+            celdaActual.setEscombros(0);
+
+            for (Personaje personaje : celdaActual.getLista_personajes()) {
+                if (personaje instanceof Minero) {
+                    Minero minero = (Minero) personaje;
+                    this.agregarEscombro(cant_escombros_esta_celda);
+                    escribidor.println(minero.getNombre() + Constantes.LLEVAR + minero.obtenerTotalEscombros() + Constantes.KILOS);
+                }
+            }
+        }
+    }
+
+
+
+    @Override
+    public String toString() {
+        return Constantes.MINERO + super.toString() + '}';
+    }
+
+    @Override
+    public boolean comprobarPuertaSalida() {
+        CentralNuclear central = CentralNuclear.getInstancia();
+        int posicion_actual = this.getIdCeldaActual();
+        Celda celdaActual = central.getCelda(Utilidad.calcularFila(posicion_actual), Utilidad.calcularColumna(posicion_actual));
+
+        return celdaActual.getPuerta() != null;
+    }
+
+
 
 }
