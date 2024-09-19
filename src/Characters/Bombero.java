11eb57b7e4477200d1594818629898d18a0a920b
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-19 11:12:36 +0200

    bombero class improved

--- src/Characters/Bombero.java
+++ src/Characters/Bombero.java
@@ -27,4 +27,62 @@ public class Bombero extends Operador {
     private void recargarRefrigerador() {
         this.bateriaRefrigerador = 5 ;
     }
+
+    public void refrigerar() {
+        CentralNuclear central = CentralNuclear.getInstancia();
+
+
+        int posicion_actual = getIdCeldaActual();
+        Celda celdaActual = central.getCelda(Utilidad.calcularFila(posicion_actual), Utilidad.calcularColumna(posicion_actual));
+        celdaActual.setRefrigerada(true);
+
+    }
+
+
+
+    @Override
+    public void realizarAcciones(PrintWriter escribidor) {
+
+        if(!comprobarPuertaSalida()) {
+            if(this.bateriaRefrigerador>0) {
+                this.mover(escribidor);
+                this.refrigerar();
+                this.bateriaRefrigerador--;
+            }else {
+                escribidor.println(getNombre() + Constantes.RECARGANDO_BATERIA);
+                this.recargarRefrigerador();
+
+            }
+
+
+        }
+    }
+
+
+
+    @Override
+    public String toString() {
+        return Constantes.BOMBERO + super.toString() +
+                Constantes.CARGA_REFRIGERADOR + bateriaRefrigerador +
+                '}';
+    }
+
+
+    @Override
+    public boolean comprobarPuertaSalida() {
+        CentralNuclear central = CentralNuclear.getInstancia();
+        int posicion_actual = this.getIdCeldaActual();
+        Celda celdaActual = central.getCelda(Utilidad.calcularFila(posicion_actual), Utilidad.calcularColumna(posicion_actual));
+
+        if(celdaActual.getPuerta()!= null) {
+            return true;
+
+        }
+        return false;
+    }
+
+
+
+
+
 }
