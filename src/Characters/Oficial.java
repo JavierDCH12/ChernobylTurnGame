750220e293f6d3a11832b9f9e53faa79cb40ab2c
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-19 11:00:39 +0200

    oficial added info

--- src/Characters/Oficial.java
+++ src/Characters/Oficial.java
@@ -20,4 +20,60 @@ public class Oficial extends KGB {
         }
 
     }
+
+    @Override
+    public String toString() {
+        return Constantes.OFICIAL + super.toString();
+    }
+
+    public void catalogar(PrintWriter escribidor) {
+        int posicion_actual = getIdCeldaActual();
+        CentralNuclear central = CentralNuclear.getInstancia();
+        Celda celdaActual = central.getCelda(Utilidad.calcularFila(posicion_actual), Utilidad.calcularColumna(posicion_actual));
+
+        for (Personaje cadapersonaje : celdaActual.getLista_personajes()) {
+            if (cadapersonaje instanceof Operador && !((Operador) cadapersonaje).isCatalogado()) {
+                String tipoOperador = "";
+                if (cadapersonaje instanceof Cientifico) {
+                    tipoOperador = Constantes.CIENTIFICO;
+                } else if (cadapersonaje instanceof Minero) {
+                    tipoOperador = Constantes.MINERO;
+                } else if (cadapersonaje instanceof Bombero) {
+                    tipoOperador = Constantes.BOMBERO;
+                } else if (cadapersonaje instanceof Oficial) {
+                    tipoOperador = Constantes.OFICIAL;
+                } else if (cadapersonaje instanceof Voluntario) {
+                    tipoOperador = Constantes.VOLUNTARIO;
+                } else if (cadapersonaje instanceof Robot) {
+                    tipoOperador = Constantes.ROBOT;
+                }
+
+                escribidor.println(getNombre() + Constantes.DOS_PUNTOS + tipoOperador + Constantes.ALMOHADILLA + cadapersonaje.getNombre() + Constantes.ALMOHADILLA + cadapersonaje.getIdCeldaActual() + Constantes.ALMOHADILLA + cadapersonaje.getMarca());
+                ((Operador) cadapersonaje).setCatalogado(true);
+            }
+        }
+    }
+
+    public void destruirLlave(PrintWriter escribidor) {
+        CentralNuclear central = CentralNuclear.getInstancia();
+        int fila = Utilidad.calcularFila(getIdCeldaActual());
+        int columna = Utilidad.calcularColumna(getIdCeldaActual());
+        Celda celdaActual;
+        celdaActual = central.getCelda(fila, columna);
+        if (celdaActual.getLlave() != null) {
+            escribidor.println(getNombre() + Constantes.LLAVE_DESTRUIDA + celdaActual.getIdentificador_celda() + Constantes.PUNTO);
+
+            celdaActual.setLlave(null);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
