f28236d85dd89c4e93fd2aef5ccaa33048727ed5
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-09-22 21:55:28 +0200

    constants added to proyect

---
+++ src/General/Constantes.java
@@ -0,0 +1,68 @@
+package General;
+
+
+public class Constantes {
+
+    public static final int FILAS = 8;
+    public static final int COLUMNAS = 8;
+    public static final String CERO = "0";
+    public static final int TURNO = 0;
+    public static final String LISTA_VACIA="Lista vacia";
+    public static final String PERSONAJE="Personaje ";
+    public static final String NO_ENCONTRADO=" no encontrado";
+    public static final String CELDAS_REFRIGERADAS="CELDAS REFRIGERADAS:";
+    public static final String LLAVE_IDLLAVE="Llave{idLlave=";
+    public static final String CELDA_ACT=", celdaAct=";
+    public static final String PUERTA_IDCELDA="Puerta [identificador_celda=";
+    public static final String ABIERTA=", abierta=";
+    public static final String ID_LLAVE=", id_llave=";
+    public static final String RECARGANDO_BATERIA=": Recargando batería refrigerador.";
+    public static final String BOMBERO="BOMBERO";
+    public static final String CARGA_REFRIGERADOR=", cargaRefrigerador=";
+    public static final String LLAVE_RECOGIDA=": Llave recogida en celda ";
+    public static final String CIENTIFICO="CIENTIFICO";
+    public static final String NOMBRE="nombre=";
+    public static final String TURNO_=", turno=";
+    public static final String ID_CELDA_ACTUAL=", idCeldaActual=";
+    public static final String MARCA=", marca=";
+    public static final String LLEVAR=": Llevo ";
+    public static final String KILOS=" kilos de escombros.";
+    public static final String MINERO="MINERO";
+    public static final String LLAVE_DESTRUIDA=": Llave destruida en celda ";
+    public static final String OFICIAL="Oficial";
+    public static final String OPERADOR_CAT="Operador [catalogado=";
+    public static final String EN_PUERTA=", enPuertaSalida=";
+    public static final String NO_PUEDO=": ¡NO PUEDO! ";
+    public static final String ESTA_EN_CELDA=" está en la celda ";
+    public static final String ES_TURNO=". Y es el turno ";
+    public static final String LA_MARCA=". La marca es: ";
+    public static final String PUNTO=".";
+    public static final String DETECTADA=": Detectadas ";
+    public static final String ZONA_INFLUENCIA=" personas en la zona de influencia. ";
+    public static final String PARENTESIS1 ="(";
+    public static final String PARENTESIS2 =")";
+    public static final String INFORMEMINEROS ="INFORME MINEROS";
+    public static final String INFORMEROBOTS ="INFORME ROBOTS";
+    public static final String INFORMESPIRIT = "INFORME Spirit";
+    public static final String INFORMEBOMBEROS = "INFORME BOMBEROS";
+    public static final String DOS_PUNTOS =": ";
+    public static final String ALMOHADILLA ="#";
+    public static final String PUERTA_ABIERTA=": ¡Puerta abierta!";
+    public static final String ESCOMBROS_RECOGIDOS =": Escombros recogidos ";
+    public static final String ROBOT="ROBOT";
+    public static final String VOLUNTARIO="VOLUNTARIO";
+    public static final String INFORME="INFORME ";
+    public static final String MENSAJEOK="TODO OK";
+    public static final String INFORMES="informes";
+    public static final String SIMULACION ="simulacionEntregaFinal";
+    public static final String TURNO_SIMUL ="TURNO: ";
+
+
+
+
+
+
+
+
+
+}
